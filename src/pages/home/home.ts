6f7de3f708afff250d43912b6e8278170f3077cd
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-08-16 22:01:58 +0200

    services now working, fetching heroes from a call

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { OnInit } from '@angular/core';
 import { Hero } from '../../models/hero';
 import { HEROES } from '../../models/mock-heroes';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [HeroService]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+	ngOnInit(): void {
+  		this.getHeroes();
+  	}
+
 	title = 'Tour of Heroes';
   //hero = 'Windstorm';
   hero: Hero = {
 	  id: 1,
 	  name: 'Windstorm'
 	};
-	heroes: Hero[] = HEROES
-  constructor(public navCtrl: NavController) {
+	heroes: Hero[];
+	getHeroes(): void {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
 
+		//Simulate the same thing above with slow connection
+		// this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+	}
+  constructor(public navCtrl: NavController, private heroService: HeroService) {
+  	//this.heroes = this.heroService.getHeroes();
   }
 
 }
