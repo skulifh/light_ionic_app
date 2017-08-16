77de16d2b294807ffde7b7f044a9a22a33b8d70c
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-08-16 21:31:09 +0200

    working copy of hero

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Hero } from '../../models/hero';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+	title = 'Tour of Heroes';
+  //hero = 'Windstorm';
+  hero: Hero = {
+	  id: 1,
+	  name: 'Windstorm'
+	};
   constructor(public navCtrl: NavController) {
 
   }
