8ca49589b3d6af6a076ec2836e22b8030b5e05c6
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-08-19 17:26:03 +0200

    cleaning up code and removing hero objects

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
-import { Hero } from '../models/hero';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
--- src/pages/movies/movies.ts
+++ src/pages/movies/movies.ts
@@ -1,48 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { OnInit } from '@angular/core';
-import { Hero } from '../../models/hero';
 import { Movie } from '../../models/movie';
-import { HEROES } from '../../models/mock-heroes';
-import { HeroService } from '../../services/hero.service';
 import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'page-movies',
   templateUrl: 'movies.html',
-  providers: [HeroService, MovieService]
+  providers: [MovieService]
 })
 export class MoviesPage implements OnInit {
 	ngOnInit(): void {
-  		this.getHeroes();
   		this.getMovies();
 
   		this.default_movies = this.movies;
   	}
-
-	title = 'Tour of Heroes';
-  //hero = 'Windstorm';
-  hero: Hero = {
-	  id: 1,
-	  name: 'Windstorm'
-	};
-	heroes: Hero[];
 	movies: Movie[];
 
-	//todo: use this instead of always call getHeroes()
+	//todo: use this instead of always call getMovies()
 	default_movies: Movie[];
 
-	getHeroes(): void {
-		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-
-		//Simulate the same thing above with slow connection
-		// this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-	}
 	getMovies(): void {
 		this.movieService.getMovies().then(movies => this.movies = movies);
-
-		//Simulate the same thing above with slow connection
-		// this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
 	}
 	searchMovies(ev: any): void {
 		let val = ev.target.value;
@@ -54,14 +33,11 @@ export class MoviesPage implements OnInit {
 	    	this.movieService.getMovies().then(movies => this.movies = movies);
 	    }
 
-		//this.movieService.searchMovies().then(movies => this.movies = movies);
-
 		//Simulate the same thing above with slow connection
 		// this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
 	}
 
-  constructor(public navCtrl: NavController, private heroService: HeroService, private movieService: MovieService) {
-  	//this.heroes = this.heroService.getHeroes();
+  constructor(public navCtrl: NavController, private movieService: MovieService) {
   }
 
 }
