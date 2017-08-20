eab53c45ea6754239ef59e11d0ad2fd93e9d8a5c
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-08-20 20:38:21 +0200

    Now checking if there is a video url available and displaying a youtube video if so

--- src/pages/movie-details/movie-details.ts
+++ src/pages/movie-details/movie-details.ts
@@ -19,10 +19,21 @@ export class MovieDetailsPage {
   		// this.url = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + this.video.key + "?rel=0&amp;controls=0&amp;showinfo=0")
   	}
 
+  buildUrl(video): SafeResourceUrl {
+    if (video) {
+      return this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + video.key + "?rel=0&amp;controls=0&amp;showinfo=0")
+    } 
+    else {
+      return null
+    }
+  }
+
 	getMovieVideos(): void {
-		this.movieService.getVideos(this.selectedMovie.id).then(video => this.url = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + video.key + "?rel=0&amp;controls=0&amp;showinfo=0"));
+		this.movieService.getVideos(this.selectedMovie.id).then(video => this.url = this.buildUrl(video));
 
 	}
+
+  
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private movieService: MovieService, private sanitizer: DomSanitizer) {
     // If we navigated to this page, we will have a movie available as a nav param
