93d7c9ef23bb6293712fd8e17839c5c34a05be9f
Skúli Freyr Hinriksson <skuli@sareye.com>
2017-08-22 21:26:13 +0200

    now handling errors from the service promises. Also moved the moviedb api path and key into a global file. Did a bit of cleaning to the service code

---
+++ src/app/globals.ts
@@ -0,0 +1,2 @@
+export const api_path = "https://api.themoviedb.org/3"
+export const api_key = "97c9873a035726c716e8254e0a0e8ed1"
--- src/services/tvshow.service.ts
+++ src/services/tvshow.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 import { Tvshow } from '../models/tvshow';
 import { Video } from '../models/video';
+import * as myGlobals from '../app/globals'
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -9,41 +10,56 @@ export class TvshowService {
 	constructor(private http: Http) {}
 
 	getTvshows(): Promise<Tvshow[]> {
-		const url = "https://api.themoviedb.org/3/tv/top_rated";
+		const url = `${myGlobals.api_path}/tv/top_rated`;
+
 	    return this.http.get(url, {
-	        params: { api_key: "97c9873a035726c716e8254e0a0e8ed1" }
-	      })
-		    .toPromise()
-		    .then(response => response.json()["results"] as Tvshow[])
-	  }
+	        params: { api_key: myGlobals.api_key }
+	    })
+	    .toPromise()
+	    .then(
+	    	function(response) {
+  				return response.json()["results"] as Tvshow[];
+  			},
+	        //Error fallback function
+	        function(error) {
+	          return null;
+	        }
+        )
+	}
 
   	searchTvshows(val): Promise<Tvshow[]> {
-		const url = "https://api.themoviedb.org/3/search/tv";
+		const url = `${myGlobals.api_path}/search/tv`;
+
 	    return this.http.get(url, {
-	        params: { api_key: "97c9873a035726c716e8254e0a0e8ed1", language: "en-US", page: 1, include_adult: false, query: val }
+	        params: { api_key: myGlobals.api_key, language: "en-US", page: 1, include_adult: false, query: val }
 	      })
-		    .toPromise()
-		    .then(response => response.json()["results"] as Tvshow[])
-	  }
+	    .toPromise()
+	    .then(
+	    	function(response) {
+				return response.json()["results"] as Tvshow[];
+			},
+	        //Error fallback function
+	        function(error) {
+	          return null;
+	        }
+        )
+	}
 
   	getVideos(val): Promise<Video[]> {
-		const url = "https://api.themoviedb.org/3/tv/" + val + "/videos";
-		// this.http.get(url, {
-	 //        params: { api_key: "97c9873a035726c716e8254e0a0e8ed1", language: "en-US" }
-	 //      })
-		//     .toPromise()
-		//     .then(response => alert(JSON.stringify(response.json()["results"][0])));
+		const url = `${myGlobals.api_path}/tv/${val}/videos`;
 
 	    return this.http.get(url, {
-	        params: { api_key: "97c9873a035726c716e8254e0a0e8ed1", language: "en-US" }
-	      })
-		    .toPromise()
-		    .then(response => response.json()["results"] as Video[])
-	  }
-// 	getHeroesSlowly(): Promise<Hero[]> {
-// 	return new Promise(resolve => {
-// 		// Simulate server latency with 2 second delay
-// 		setTimeout(() => resolve(this.getHeroes()), 2000);
-// 	});
-// }
+        	params: { api_key: myGlobals.api_key, language: "en-US" }
+	    })
+	    .toPromise()
+	    .then(
+	    	function(response) {
+				return response.json()["results"] as Video[];
+			},
+	        //Error fallback function
+	        function(error) {
+	      		return null;
+	        }
+        )
+	}
 }
